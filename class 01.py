f6b04b92d479b9448e85b97ea39b899f9750d6f2
Ravi Ranjan <raviranjan9601@gmail.com>
2025-11-01 22:09:17 +0530

    Add files via upload

---
+++ class 01.py
@@ -0,0 +1,10 @@
+# this is day frist of python coding class.
+print("hello world")
+print("welcome to python programming class day 01")
+print("today, i will learn about python basics")
+print("today, i will learn 5W things in python like what, why, when, where, and who")
+
+## 5W thing in python.
+print("What is python?")
+
+print("python is a high-level ")
